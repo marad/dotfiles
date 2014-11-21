dd26f2010e550fe1eaf66504f6ec25281af6f120
morti <moriturius@gmail.com>
2014-11-21 08:38:47 +0100

    Added .gitignore_global

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ echo $DIR
 rm -rf $HOME/.vim
 rm -rf $HOME/.vimrc
 rm -rf $HOME/.gitconfig
+rm -rf $HOME/.gitignore_global
 
 
 ln -s $DIR/.vim $HOME/.vim
 ln -s $DIR/.vimrc $HOME/.vimrc
 ln -s $DIR/.gitconfig $HOME/.gitconfig
+ln -s $DIR/.gitignore_global $HOME/.gitignore_global
 
