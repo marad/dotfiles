61d0442f3cc865b011c0e6cc256b5e8c5c455169
Marcin Radoszewski <marcin.radoszewski@allegrogroup.com>
2015-11-23 11:30:44 +0100

    Config update

--- i3/xinput.sh
+++ i3/xinput.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 xinput --set-prop 13 "Synaptics Two-Finger Scrolling" 1 1      
-xinput --set-prop 13 "Two-Finger Pressure" 100
-xinput --set-prop 13 "Synaptics Finger" 20 30 257
+xinput --set-prop 13 "Synaptics Two-Finger Pressure" 100
+xinput --set-prop 13 "Synaptics Finger" 10 25 257
 
