22332d3643c379f71e91c448e5121bc08a0b1a09
Marcin Radoszewski <marcin.radoszewski@allegrogroup.com>
2015-12-22 07:09:49 +0100

    Added bspwm and sxhkd configs

---
+++ bspwm/xinput.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xinput --set-prop "AlpsPS/2 ALPS GlidePoint" "Synaptics Two-Finger Scrolling" 1 1      
+xinput --set-prop "AlpsPS/2 ALPS GlidePoint" "Synaptics Two-Finger Pressure" 100
+xinput --set-prop "AlpsPS/2 ALPS GlidePoint" "Synaptics Finger" 10 25 257
+
